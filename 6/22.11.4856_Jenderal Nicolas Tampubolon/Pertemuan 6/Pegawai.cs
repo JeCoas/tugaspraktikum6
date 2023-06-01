ce9f1bde2b01d252808eee5a95e63c7c372950e3
JeCoas <127827888+JeCoas@users.noreply.github.com>
2023-06-01 12:23:27 +0700

    Add files via upload

---
+++ 6/22.11.4856_Jenderal Nicolas Tampubolon/Pertemuan 6/Pegawai.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pertemuan6
+{
+	internal class Pegawai
+	{
+		//Fields
+		public string nama;
+		public string asal;
+		public string devisi;
+		public int umur;
+		public string ID;
+
+		//Constructor
+		public Pegawai(string nama, string asal, string devisi, int umur, string ID)
+		{
+			this.nama = nama;
+			this.asal = asal;
+			this.devisi = devisi;
+			this.umur = umur;
+			this.ID = ID;	
+		}
+		public Pegawai()
+		{
+
+		}
+
+		//Method
+		public void SetDataDiri(string nama, int umur, string asal)
+		{
+			this.nama = nama;
+			this.umur = umur;
+			this.asal = asal;
+			Console.WriteLine("Nama Pegawai : {0}", this.nama);
+			Console.WriteLine("Umur Pegawai : {0}", this.umur);
+			Console.WriteLine("Asal Kota    : {0}", this.asal); 
+		}
+        public void SetDataPekerja(string devisi, string ID)
+        {
+            this.devisi = devisi;
+            this.ID = ID;
+            Console.WriteLine("Devisi     : {0}", this.devisi);
+            Console.WriteLine("ID Pegawai : {0}", this.ID);
+			Console.ReadKey();
+        }
+    }
+}
---
+++ 6/22.11.4856_Jenderal Nicolas Tampubolon/Pertemuan 6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pertemuan6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Program Data Pegawai KPWS ");
+            Console.WriteLine("===========================");
+
+           Pegawai Pegawai1 = new Pegawai();
+           Pegawai Pegawai2 = new Pegawai();
+           Pegawai Pegawai3 = new Pegawai();
+
+            Pegawai1.SetDataDiri("Gugun Wahid", 23, "Semarang");
+            Pegawai1.SetDataPekerja("Marketing", "M1882672");
+            Console.WriteLine();
+
+            Pegawai2.SetDataDiri("Indah Pertiwi", 25, "Yogyakarta");
+            Pegawai2.SetDataPekerja("Admin", "A1882322");
+            Console.WriteLine();
+
+            Pegawai3.SetDataDiri("Regar Hangkasa", 31, "Medan");
+            Pegawai3.SetDataPekerja("HRD", "HRD00001");
+            Console.WriteLine();
+        }
+    }
+}
